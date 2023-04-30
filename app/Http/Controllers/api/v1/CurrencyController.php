92a0ebad7b10688f3b9d1d0d18f7e3ad82872070
Konrad Duda <konrad.duda121@gmail.com>
2023-04-30 18:49:53 +0200

    Basic currency data implementation

---
+++ app/Http/Controllers/api/v1/CurrencyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Currency;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Currency::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Currency::create($request->all());
+        return response()->json(['status' => 'success', 'msg' => "Pomyślnie dodano dane na temat waluty."], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Currency  $currency
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Currency $currency)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Currency  $currency
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Currency $currency)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Currency  $currency
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Currency $currency)
+    {
+        //
+    }
+}
